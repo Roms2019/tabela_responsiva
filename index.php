d730443f7560cd0ba2485d794fab8a3ee1590ace
Roms <54295076+Roms2019@users.noreply.github.com>
2020-02-26 23:17:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewpoint" content="width=device-width, user-scalable-no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tabelas Responsivas</title>
+    <link rel="stylesheet" href="style.min.css">
+    </head>
+<body>
+    <div class="content">
+    <table class="rTable">
+        <thread>
+            <tr>
+                <th>id</th>
+                <th>Nome</th>
+                <th>Sexo</th>
+                <th>Idade</th>
+                <th>Email</th>
+            </tr>    
+
+        </thread>  
+        <tbody>
+            <tr>  
+            <td>1</td>
+                <td>Thiago</td>
+                <td>Masculino</td>
+                <td>20</td>
+                <td>thiago@gmail.com</td>
+            </tr>
+            <tr>  
+            <td>2</td>
+                <td>Marina</td>
+                <td>Feminino</td>
+                <td>22</td>
+                <td>marina.pereira@gmail.com</td>
+            </tr>
+
+
+    </table>
+    </div>
+</body>
+</html>        
